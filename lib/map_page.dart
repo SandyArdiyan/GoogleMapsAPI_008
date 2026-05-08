c60f45d9f249e0758e80f976b55c00cc5a58ce32
SandyArdiyan <sanardiyan881@gmail.com>
2026-05-08 10:24:42 +0700

    Membuat map dart dan seting kamera

---
+++ lib/map_page.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _ctrl = Completer();
+  Marker? _pickedMarker;
+  String? _pickedAddress;
+  String? _currentAddress;
+  CameraPosition? _initialCamera;
+  Position? _currentPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _setupLocation();
+  }
+
+  Future<void> _setupLocation() async {
+    try {
+      final pos = await _determinePosition(); 
+      _currentPosition = pos;
+      _initialCamera = CameraPosition(
+        target: LatLng(pos.latitude, pos.longitude),
+        zoom: 16,
+      ); // CameraPosition
+
+      final placemarks = await placemarkFromCoordinates(
+        _currentPosition!.latitude,
+        _currentPosition!.longitude,
+      );
